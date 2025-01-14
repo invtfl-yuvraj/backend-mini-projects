e0217ccbe84e107a77f291775bfac3c8de13d058
invtfl-yuvraj <yuvrajsingh3715@gmail.com>
2025-01-14 08:18:03 +0530

    Succefully create the remaining edit and delete part of our profile maker app

--- backend-02-profile-maker/app.js
+++ backend-02-profile-maker/app.js
@@ -29,7 +29,46 @@ app.post("/create", async (req, res) => {
         image
     })
 
-    res.send(createdUser);
+    res.redirect("/users");
+});
+
+app.get("/delete/:id", async(req, res) => {
+    await userModel.findOneAndDelete({
+        _id : req.params.id
+    });
+    res.redirect("/users");
 })
+
+
+app.get("/edit/:id", async (req, res) => {
+
+    let user = await userModel.findOne({
+        _id : req.params.id
+    })
+
+    res.render("edit", {user : user});
+
+})
+
+app.post("/update/:userid", async (req, res) => {
+
+    
+    let {name, email, image} = req.body;
+
+    let updatedUser = await userModel.findOneAndUpdate({
+        _id : req.params.userid
+    },
+    {
+        name,
+        email,
+        image
+    }, 
+    {
+        new : true
+    })
+
+    res.redirect("/users");
+});
+
 
 app.listen("3000");
