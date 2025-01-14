38c69ee770631c11c92e28fbb1ee03214f341c01
invtfl-yuvraj <yuvrajsingh3715@gmail.com>
2025-01-14 07:12:16 +0530

    Created a profile using basics of MongoDB, create and read functionality as of now

---
+++ backend-02-profile-maker/app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const userModel = require("./models/user");
+
+app.set("view engine", "ejs" );
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, "public")));
+
+
+app.get("/" , (req, res) => {
+    res.render("index");
+})
+
+app.get("/users", async (req, res) => {
+
+    let allUsers = await userModel.find();
+
+    res.render("users", {users : allUsers});
+})
+
+app.post("/create", async (req, res) => {
+    let {name, email, image} = req.body;
+
+    let createdUser = await userModel.create({
+        name,
+        email,
+        image
+    })
+
+    res.send(createdUser);
+})
+
+app.listen("3000");
